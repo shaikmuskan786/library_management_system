875d9510c2e90a87f39b7e29369b3749e19c5054
Shaik Muskan <skmuskan401@gmail.com>
2024-08-27 12:20:34 +0530

    LibraryManagementSystem

---
+++ src/main/java/com/library/entity/Book.java
@@ -0,0 +1,33 @@
+package com.library.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String title;
+    private String author;
+    private boolean borrowed;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User borrowedBy;
+
+
+    public boolean isBorrowed() {
+        return borrowed;
+    }
+
+    public void setBorrowedBy(User user) {
+    }
+
+    public void setBorrowed(boolean b) {
+    }
+}
---
+++ src/main/java/com/library/entity/User.java
@@ -0,0 +1,18 @@
+package com.library.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+
+}
